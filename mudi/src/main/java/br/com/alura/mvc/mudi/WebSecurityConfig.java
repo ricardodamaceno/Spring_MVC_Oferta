5ea9ec3327d63dfb74baa677b7468ecfe843c551
Ricardo da Maceno <99434415+ricardodamaceno@users.noreply.github.com>
2022-08-15 16:42:03 -0300

    Add files via upload

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package br.com.alura.mvc.mudi;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity //isso é para habilitar o cache
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/home/**") //aqui é para tornar a home uma página publica
+			.permitAll()
+		.anyRequest()
+			.authenticated()
+		.and()
+		.formLogin(form -> form
+			.loginPage("/login")
+			.permitAll()
+			.defaultSuccessUrl("/usuario/pedido", true) //isso aqui é pra sempre redirecionar para home quando der logout
+		)
+		.logout(logout ->
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home"));
+		
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		UserDetails user =
+				 User.builder()
+					.username("maria")
+					.password(encoder.encode("maria"))
+					.roles("ADM")
+					.build();
+		
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(encoder);
+		//.withUser(user);
+	}
+	
+}
